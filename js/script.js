701267bb75ec57ca028c999c435528386d597e64
Kaioob4rreto <123511836+KaiooBarreto@users.noreply.github.com>
2023-05-15 17:23:51 -0300

    primeiro passo com js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const nome = prompt ('Olá qual o seu nome ?')
+alert (`A entt seu nome é ${nome}`)
+alert('agora me faça um favor')
+let pergunta1 = prompt('Digite um número');
+let pergunta2 = prompt('Digite outro número');
+alert(`O primeiro número que você disse foi ${pergunta1} e o segundo foi ${pergunta2}`)
+pergunta1 = parseInt(pergunta1);
+pergunta2 = parseInt(pergunta2);
+const resposta1 = pergunta1 + pergunta2;
+alert(`A junção dos dois é ${resposta1}`)
+alert('muito obrigado, era apenas isso mesmo')
